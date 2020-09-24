3eab9cb96c5dd15eaf73f2f7df9b877f5303a57f
Zeroitman <borsh130894@gmail.com>
2020-09-24 23:47:57 +0600

    Added post list and create post functional

--- social_network/urls.py
+++ social_network/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from social_network.views import PostList, UserListView, UserDetailView, change_access_status, change_create_post_status
+from social_network.views import \
+    (PostList, PostCreateView, PostDetailView,
+     UserListView, UserDetailView,
+     change_access_status, change_create_post_status)
 
 urlpatterns = [
     path('post-list', PostList.as_view(), name='post_list'),
+    path('post-create', PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/detail', PostDetailView.as_view(), name='post_detail'),
     path('users-list/', UserListView.as_view(), name='users_list'),
-    path('user/<int:pk>/detail/', UserDetailView.as_view(), name='user_detail'),
+    path('user/<int:pk>/detail', UserDetailView.as_view(), name='user_detail'),
     path('user/<int:pk>/change_access_status', change_access_status, name='change_access_status'),
     path('user/<int:pk>/change_create_post_status', change_create_post_status, name='change_create_post_status'),
 ]
