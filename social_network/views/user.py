46db73e539de93bf2c7fb1cdd46eb44833ec1b16
Zeroitman <borsh130894@gmail.com>
2020-09-30 17:42:44 +0600

    Added count of user posts

--- social_network/views/user.py
+++ social_network/views/user.py
@@ -14,6 +14,14 @@ class UserListView(ListView, LoginRequiredMixin):
     model = UserInfo
     template_name = 'user/users_list.html'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        users_posts = {}
+        for a in UserInfo.objects.all():
+            users_posts[a.pk] = len(Post.objects.filter(post_user=a.pk))
+        context['post_count'] = users_posts
+        return context
+
 
 class UserDetailView(DetailView, LoginRequiredMixin):
     model = UserInfo
