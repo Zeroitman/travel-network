9312df8caf8b8ce87871911226dba23505a5ba1c
Zeroitman <borsh130894@gmail.com>
2020-09-30 14:08:03 +0600

    Fixed comments blog

--- source/urls.py
+++ source/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from social_network.views import register
+from social_network.views.user import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', register, name='register'),
     path('travel-blog/', include('social_network.urls')),
     path('auth/', include('django.contrib.auth.urls'))
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
               + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
